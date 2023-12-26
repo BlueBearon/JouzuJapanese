19084cb48fd05e6f5ac843c29a1c098a1adb7b8c
Chase-Packer <cdpacker@wm.edu>
2023-12-26 07:22:49 -0500

    Initial javascript

---
+++ jouzujapanese/frontend/NavigationBar.js
@@ -0,0 +1,26 @@
+var hiragana = document.getElementById("toHiraganaTab")
+var katakana = document.getElementById("toKatakanaTab")
+var verb = document.getElementById("toVerbConjugationTab")
+var adjective = document.getElementById("toAdjectiveConjugationTab")
+
+verb.onclick = function() {
+    //open frontend/PrototypeConjugationPractice.html
+    window.location.href = "PrototypeConjugationPractice.html";
+}
+
+adjective.onclick = function() {
+    //open frontend/PrototypeConjugationPractice.html
+    window.location.href = "PrototypeConjugationPractice.html";
+}
+
+hiragana.onclick = function() {
+    //open D:\Coding\Jouzu\JouzuJapanese\jouzujapanese\frontend\PrototypeKanaPractice.html
+    window.location.href = "PrototypeKanaPractice.html";
+}
+
+katakana.onclick = function() {
+    //open D:\Coding\Jouzu\JouzuJapanese\jouzujapanese\frontend\PrototypeKanaPractice.html
+    window.location.href = "PrototypeKanaPractice.html";
+}
+
+
